208febebfabccb89e377f6bbd642d3b93dbf84f7
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-04-21 20:38:16 +0000

    Adding a detail page for grams

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Gramz::Application.routes.draw do
   devise_for :users
   root "grams#index"
 
-  resources :grams
+  resources :grams, only: [:new, :create, :show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
